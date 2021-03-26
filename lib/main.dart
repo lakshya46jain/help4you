9c4fdf42132c4894e7cc7ae7c67053e5931f5426
Lakshya Jain <ljain04062005@outlook.com>
2021-03-26 20:11:53 +0530

    Configured Firebase for Android, iOS and Flutter Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
 // Dependency Imports
+import 'package:firebase_core/firebase_core.dart';
 // File Imports
 import 'package:help4you/screens/welcome_screen/welcome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
