ed75f0daf1585b678746aa9e2af0e7d75b8d32e6
Lakshya Jain <ljain04062005@outlook.com>
2021-03-26 19:08:33 +0530

    Created Signature Button Template

---
+++ lib/constants/signature_button.dart
@@ -0,0 +1,64 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+// Dependency Imports
+// File Imports
+
+class SignatureButton extends StatelessWidget {
+  final Function onTap;
+  final String text;
+  final IconData icon;
+  SignatureButton({
+    this.onTap,
+    this.text,
+    this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      padding: EdgeInsets.all(0),
+      onPressed: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.deepOrangeAccent,
+        ),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height / (1792 / 120),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 20.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / (828 / 20),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
