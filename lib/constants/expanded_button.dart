1d6549d44a2f6331e26a7c27ac04329e152d8a23
Lakshya Jain <ljain04062005@outlook.com>
2021-03-27 20:00:14 +0530

    Created Expanded Button Template

---
+++ lib/constants/expanded_button.dart
@@ -0,0 +1,65 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+// Dependency Imports
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// File Imports
+
+class ExpandedButton extends StatelessWidget {
+  final Function onPressed;
+  final IconData icon;
+  final String text;
+  ExpandedButton({
+    this.onPressed,
+    this.icon,
+    this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 7.5,
+        horizontal: 10.0,
+      ),
+      child: MaterialButton(
+        padding: EdgeInsets.all(0.0),
+        onPressed: onPressed,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 10.0,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color(0xFFF5F6F9),
+          ),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                size: 30.0,
+                color: Colors.deepOrange,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / (828 / 40),
+              ),
+              Expanded(
+                child: Text(
+                  text,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Icon(
+                FontAwesomeIcons.chevronRight,
+                color: Colors.black,
+                size: 25.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
