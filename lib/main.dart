3d332f6113b8488177cec6b45a2e56778195130d
Lakshya Jain <ljain04062005@outlook.com>
2021-03-28 01:05:37 +0530

    Added & Implemented Rate My App Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // Dependency Imports
 import 'package:provider/provider.dart';
+import 'package:rate_my_app/rate_my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 // File Imports
 import 'package:help4you/services/auth.dart';
@@ -14,7 +16,64 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  RateMyApp _rateMyApp = RateMyApp(
+    minDays: 7,
+    minLaunches: 10,
+    remindDays: 7,
+    remindLaunches: 10,
+    googlePlayIdentifier: '',
+    appStoreIdentifier: '',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    // Rate My App Feature
+    _rateMyApp.init().then(
+      (_) {
+        if (_rateMyApp.shouldOpenDialog) {
+          _rateMyApp.showStarRateDialog(
+            context,
+            title: 'Rate Help4You',
+            message:
+                'Did you like your experience with Help4You? Then take a little bit of your time to leave a rating:',
+            actionsBuilder: (context, stars) {
+              return [
+                TextButton(
+                  child: Text('Ok'),
+                  onPressed: () async {
+                    HapticFeedback.lightImpact();
+                    await _rateMyApp
+                        .callEvent(RateMyAppEventType.rateButtonPressed);
+                    Navigator.pop<RateMyAppDialogButton>(
+                      context,
+                      RateMyAppDialogButton.rate,
+                    );
+                  },
+                ),
+              ];
+            },
+            dialogStyle: DialogStyle(
+              titleAlign: TextAlign.center,
+              messageAlign: TextAlign.center,
+              messagePadding: EdgeInsets.only(bottom: 20),
+            ),
+            starRatingOptions: StarRatingOptions(),
+            onDismissed: () => _rateMyApp.callEvent(
+              RateMyAppEventType.laterButtonPressed,
+            ),
+          );
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
