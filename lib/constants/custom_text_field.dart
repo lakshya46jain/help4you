23b2742a882e24b2bc6834411472c4749a54755d
Lakshya Jain <ljain04062005@outlook.com>
2021-03-27 13:36:58 +0530

    Created Custom Text Field Template

---
+++ lib/constants/custom_text_field.dart
@@ -0,0 +1,63 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+// Dependency Imports
+// File Imports
+
+class CustomTextField extends StatelessWidget {
+  final TextInputType keyboardType;
+  final int maxLines;
+  final Function validator;
+  final Function onChanged;
+  final String initialValue;
+  final String labelText;
+  final String hintText;
+
+  CustomTextField({
+    @required this.keyboardType,
+    this.maxLines,
+    this.validator,
+    this.onChanged,
+    this.initialValue,
+    this.labelText,
+    this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: keyboardType,
+      maxLines: maxLines,
+      validator: validator,
+      initialValue: initialValue,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(
+          color: Colors.grey,
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Colors.grey,
+        ),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        contentPadding: EdgeInsets.all(20.0),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Colors.red),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Colors.red),
+        ),
+      ),
+    );
+  }
+}
