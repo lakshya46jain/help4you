479fd222aeedb205619d8f06885db8fa788d83cc
Lakshya Jain <ljain04062005@outlook.com>
2021-04-01 10:59:55 +0530

    Created Custom Media Buttons

---
+++ lib/constants/custom_media_button.dart
@@ -0,0 +1,60 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+// Dependency Imports
+// File Imports
+
+class CustomMediaButton extends StatelessWidget {
+  final Function onTap;
+  final IconData icon;
+  final String text;
+
+  CustomMediaButton({
+    @required this.onTap,
+    @required this.icon,
+    @required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.deepOrangeAccent,
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 27.0,
+                  ),
+                ),
+                SizedBox(width: 5),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
