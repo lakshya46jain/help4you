aabe0b747c6c18071c6ceae3ff5f0fecb85413a6
Lakshya Jain <ljain04062005@outlook.com>
2021-04-21 19:40:53 +0530

    Removed Unused Filtering & Condition Statements

--- lib/screens/edit_profile_screen/stream_builder.dart
+++ lib/screens/edit_profile_screen/stream_builder.dart
@@ -9,7 +9,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 // File Imports
 import 'package:help4you/models/user_model.dart';
 import 'package:help4you/services/database.dart';
-import 'package:help4you/constants/loading.dart';
 import 'package:help4you/constants/custom_text_field.dart';
 import 'package:help4you/constants/phone_number_field.dart';
 
@@ -43,141 +42,136 @@ class EditProfileStreamBuilder extends StatelessWidget {
       child: StreamBuilder(
         stream: DatabaseService(uid: user.uid).userData,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserDataCustomer userData = snapshot.data;
-            return Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / (1792 / 230),
-                  width: MediaQuery.of(context).size.width / (828 / 230),
-                  child: Stack(
-                    fit: StackFit.expand,
-                    clipBehavior: Clip.none,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: SizedBox(
-                          child: ClipOval(
-                            child: (imageFile != null)
-                                ? Image.file(
-                                    imageFile,
-                                    fit: BoxFit.fill,
-                                  )
-                                : CachedNetworkImage(
-                                    imageUrl: userData.profilePicture,
-                                    fit: BoxFit.fill,
-                                  ),
-                          ),
+          UserDataCustomer userData = snapshot.data;
+          return Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / (1792 / 230),
+                width: MediaQuery.of(context).size.width / (828 / 230),
+                child: Stack(
+                  fit: StackFit.expand,
+                  clipBehavior: Clip.none,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: SizedBox(
+                        child: ClipOval(
+                          child: (imageFile != null)
+                              ? Image.file(
+                                  imageFile,
+                                  fit: BoxFit.fill,
+                                )
+                              : CachedNetworkImage(
+                                  imageUrl: userData.profilePicture,
+                                  fit: BoxFit.fill,
+                                ),
                         ),
                       ),
-                      Positioned(
-                        right: -12,
-                        bottom: 0,
-                        child: SizedBox(
-                          height:
-                              MediaQuery.of(context).size.height / (1792 / 92),
-                          width: MediaQuery.of(context).size.width / (828 / 92),
-                          child: GestureDetector(
-                            onTap: () {
-                              final pickerOptions = CupertinoActionSheet(
-                                title: Text("Profile Picture"),
-                                message: Text(
-                                  "Please select how you want to upload the profile picture",
-                                ),
-                                actions: [
-                                  CupertinoActionSheetAction(
-                                    onPressed: onPressed1,
-                                    child: Text(
-                                      "Camera",
-                                    ),
-                                  ),
-                                  CupertinoActionSheetAction(
-                                    onPressed: onPressed2,
-                                    child: Text(
-                                      "Gallery",
-                                    ),
-                                  ),
-                                ],
-                                cancelButton: CupertinoActionSheetAction(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
+                    ),
+                    Positioned(
+                      right: -12,
+                      bottom: 0,
+                      child: SizedBox(
+                        height:
+                            MediaQuery.of(context).size.height / (1792 / 92),
+                        width: MediaQuery.of(context).size.width / (828 / 92),
+                        child: GestureDetector(
+                          onTap: () {
+                            final pickerOptions = CupertinoActionSheet(
+                              title: Text("Profile Picture"),
+                              message: Text(
+                                "Please select how you want to upload the profile picture",
+                              ),
+                              actions: [
+                                CupertinoActionSheetAction(
+                                  onPressed: onPressed1,
                                   child: Text(
-                                    "Cancel",
+                                    "Camera",
                                   ),
                                 ),
-                              );
-                              showCupertinoModalPopup(
-                                context: context,
-                                builder: (BuildContext context) =>
-                                    pickerOptions,
-                              );
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Color(0xFFF5F6F9),
-                                border: Border.all(
-                                  color: Colors.white,
+                                CupertinoActionSheetAction(
+                                  onPressed: onPressed2,
+                                  child: Text(
+                                    "Gallery",
+                                  ),
+                                ),
+                              ],
+                              cancelButton: CupertinoActionSheetAction(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  "Cancel",
                                 ),
                               ),
-                              child: Icon(
-                                FluentIcons.camera_24_regular,
-                                color: Colors.black,
+                            );
+                            showCupertinoModalPopup(
+                              context: context,
+                              builder: (BuildContext context) => pickerOptions,
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFF5F6F9),
+                              border: Border.all(
+                                color: Colors.white,
                               ),
+                            ),
+                            child: Icon(
+                              FluentIcons.camera_24_regular,
+                              color: Colors.black,
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / (1792 / 75),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / (1792 / 75),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                  vertical: 10.0,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 15.0,
-                    vertical: 10.0,
-                  ),
-                  child: CustomTextField(
-                    keyboardType: TextInputType.name,
-                    labelText: "Full Name",
-                    hintText: "Enter full name",
-                    initialValue: userData.fullName,
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return "Name field cannot be empty";
-                      } else if (value.length < 2) {
-                        return "Name must be atleast 2 characters long";
-                      } else {
-                        return null;
-                      }
-                    },
-                    onChanged: onChanged,
-                  ),
+                child: CustomTextField(
+                  keyboardType: TextInputType.name,
+                  labelText: "Full Name",
+                  hintText: "Enter full name",
+                  initialValue: userData.fullName,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Name field cannot be empty";
+                    } else if (value.length < 2) {
+                      return "Name must be atleast 2 characters long";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: onChanged,
                 ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / (1792 / 30),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / (1792 / 30),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15.0,
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 15.0,
-                  ),
-                  child: PhoneNumberTextField(
-                    phoneIsoCode: userData.phoneIsoCode,
-                    nonInternationalNumber: userData.nonInternationalNumber,
-                    onPhoneNumberChange: onPhoneNumberChange,
-                  ),
+                child: PhoneNumberTextField(
+                  phoneIsoCode: userData.phoneIsoCode,
+                  nonInternationalNumber: userData.nonInternationalNumber,
+                  onPhoneNumberChange: onPhoneNumberChange,
                 ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / (1792 / 30),
-                ),
-              ],
-            );
-          } else {
-            return PouringHourGlassPageLoad();
-          }
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / (1792 / 30),
+              ),
+            ],
+          );
         },
       ),
     );
--- lib/screens/professionals_details/service_stream_builder.dart
+++ lib/screens/professionals_details/service_stream_builder.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 // Dependency Imports
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:help4you/constants/loading.dart';
 // File Imports
 import 'package:help4you/screens/home_screen/service_tiles.dart';
 
@@ -21,34 +20,25 @@ class ServiceTileBuilder extends StatelessWidget {
           .doc(uid)
           .collection("Services")
           .where("Visibility", isEqualTo: true)
-          .where("User UID", isEqualTo: uid)
           .snapshots(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return SingleChildScrollView(
-            child: ListView.builder(
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              itemCount: snapshot.data.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
-                return ServiceTiles(
-                  professionalUID: uid,
-                  docId: documentSnapshot.id,
-                  serviceTitle: documentSnapshot["Service Title"],
-                  serviceDescription: documentSnapshot["Service Description"],
-                  servicePrice: documentSnapshot["Service Price"],
-                );
-              },
-            ),
-          );
-        } else if (!snapshot.hasData) {
-          return Center(
-            child: Text("No Services Provided By The User"),
-          );
-        } else {
-          return PouringHourGlassPageLoad();
-        }
+        return SingleChildScrollView(
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+              return ServiceTiles(
+                professionalUID: uid,
+                docId: documentSnapshot.id,
+                serviceTitle: documentSnapshot["Service Title"],
+                serviceDescription: documentSnapshot["Service Description"],
+                servicePrice: documentSnapshot["Service Price"],
+              );
+            },
+          ),
+        );
       },
     );
   }
