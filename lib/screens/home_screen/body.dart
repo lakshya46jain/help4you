4b97fe899dbccda326133d1531ccae5c1dbf602f
Lakshya Jain <ljain04062005@outlook.com>
2021-04-22 23:30:39 +0530

    Implemented Occupation List Builder (Home Screen)

--- lib/screens/home_screen/body.dart
+++ lib/screens/home_screen/body.dart
@@ -2,10 +2,46 @@
 import 'package:flutter/material.dart';
 // Dependency Imports
 // File Imports
+import 'package:help4you/screens/home_screen/occupation_list_builder.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Services",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    // TODO: Navigate To See All Page
+                  },
+                  child: Text(
+                    "See All",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          OccupationListBuilder(),
+        ],
+      ),
+    );
   }
 }
