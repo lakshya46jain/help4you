11a14f2ddcd25f61cb3b50f65cdf04f57a68b411
Lakshya Jain <ljain04062005@outlook.com>
2021-04-22 23:41:37 +0530

    Connected Cart Body To Database & Started Retrieving Data

--- lib/screens/cart_screen/body.dart
+++ lib/screens/cart_screen/body.dart
@@ -1,11 +1,42 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
 // Dependency Imports
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 // File Imports
+import 'package:help4you/models/user_model.dart';
+import 'package:help4you/screens/cart_screen/cart_service_tile.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Get User UID
+    final user = Provider.of<Help4YouUser>(context);
+
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("H4Y Users Database")
+          .doc(user.uid)
+          .collection("Cart")
+          .snapshots(),
+      builder: (context, snapshot) {
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          itemCount: snapshot.data.docs.length,
+          itemBuilder: (context, index) {
+            DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+            return CartServiceTile(
+              customerUID: user.uid,
+              docId: documentSnapshot.id,
+              serviceTitle: documentSnapshot["Service Title"],
+              servicePrice: documentSnapshot["Service Price"],
+              professionalUID: documentSnapshot["Professional UID"],
+              serviceDescription: documentSnapshot["Service Description"],
+            );
+          },
+        );
+      },
+    );
   }
 }
