51635aa157935cf1ddcfb9531d63e53d3e3a7f3e
lakshyaj46 <lakshyaj465@gmail.com>
2021-06-23 23:45:02 +0530

    Redesigned Settings Screen User Interface

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,9 +1,16 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
 // Dependency Imports
+import 'package:share/share.dart';
+import 'package:provider/provider.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 // File Imports
-import 'package:help4you/screens/settings_screen/body.dart';
-import 'package:help4you/screens/settings_screen/app_bar.dart';
+import 'package:help4you/services/auth.dart';
+import 'package:help4you/models/user_model.dart';
+import 'package:help4you/constants/expanded_button.dart';
+import 'package:help4you/screens/about_us_screen/about_us.dart';
+import 'package:help4you/screens/settings_screen/stream_builder.dart';
+import 'package:help4you/screens/edit_profile_screen/edit_profile_screen.dart';
 
 class SettingsScreens extends StatefulWidget {
   @override
@@ -13,18 +20,103 @@ class SettingsScreens extends StatefulWidget {
 class _SettingsScreensState extends State<SettingsScreens> {
   @override
   Widget build(BuildContext context) {
+    // Get User
+    final user = Provider.of<Help4YouUser>(context);
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(
-            MediaQuery.of(context).size.height / (1792 / 100),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (user != null) ...[
+                SizedBox(
+                  height: 70.0,
+                ),
+                ProfileCardStreamBuilder(),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 20.0, bottom: 5.0, top: 15.0, left: 20.0),
+                  child: Divider(
+                    thickness: 1.0,
+                    color: Colors.grey[300],
+                  ),
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.info_24_regular,
+                  text: "Our Handbook",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AboutUs(),
+                      ),
+                    );
+                  },
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                  child: Divider(
+                    thickness: 1.0,
+                    color: Colors.grey[300],
+                  ),
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.person_24_regular,
+                  text: "Personal Data",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditProfileScreen(),
+                      ),
+                    );
+                  },
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.sign_out_24_regular,
+                  text: "Sign Out",
+                  onPressed: () {
+                    return AuthService().signOut();
+                  },
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                  child: Divider(
+                    thickness: 1.0,
+                    color: Colors.grey[300],
+                  ),
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.star_24_regular,
+                  text: "Rate Us",
+                  onPressed: () {},
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.person_feedback_24_regular,
+                  text: "Feedback",
+                  onPressed: () {},
+                ),
+                ExpandedButton(
+                  icon: FluentIcons.share_24_regular,
+                  text: "Share Help4You",
+                  onPressed: () {
+                    Share.share(
+                      "Have you tried the Help4You app? It's simple to book services like appliance repair, electricians, gardeners & more...\nTo download our app please visit https://www.help4you.webflow.io/download",
+                      subject: "Try Help4You",
+                    );
+                  },
+                ),
+              ] else ...[
+                Container(),
+              ],
+            ],
           ),
-          child: SettingsAppBar(),
         ),
-        body: Body(),
       ),
     );
   }
--- lib/screens/settings_screen/stream_builder.dart
+++ lib/screens/settings_screen/stream_builder.dart
@@ -18,104 +18,60 @@ class ProfileCardStreamBuilder extends StatelessWidget {
       builder: (context, snapshot) {
         UserDataCustomer userData = snapshot.data;
         return Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Stack(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+          child: Row(
             children: [
-              Padding(
-                padding: EdgeInsets.only(left: 35.0),
-                child: Container(
-                  height: 140.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 15),
-                        blurRadius: 20.0,
-                        color: Color(0xFFDADADA),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 95.0,
-                      top: 15.0,
-                      bottom: 15.0,
-                      right: 10.0,
+              Container(
+                height: 75.0,
+                width: 75.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.transparent,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 15),
+                      blurRadius: 20.0,
+                      color: Color(0xFFDADADA),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          userData.fullName,
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 3,
-                        ),
-                        Text(
-                          userData.phoneNumber,
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          "Member Since",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 2.5,
-                        ),
-                        Text(
-                          "12 July 2020",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                          ),
-                        ),
-                      ],
-                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: CachedNetworkImage(
+                    height: 75.0,
+                    width: 75.0,
+                    imageUrl: userData.profilePicture,
                   ),
                 ),
               ),
-              Positioned(
-                top: 15,
-                child: SizedBox(
-                  height: 110,
-                  width: 110,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: Color(0xFFF5F6F9),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0, 15),
-                          blurRadius: 20.0,
-                          color: Color(0xFFDADADA),
-                        ),
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: CachedNetworkImage(
-                        imageUrl: userData.profilePicture,
-                        fit: BoxFit.fill,
-                      ),
+              SizedBox(
+                width: 15.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    userData.fullName,
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 25.0,
+                      color: Colors.black,
+                      fontFamily: "BalooPaaji",
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
-                ),
+                  Text(
+                    userData.phoneNumber,
+                    style: TextStyle(
+                      height: 1.0,
+                      fontSize: 18.0,
+                      color: Colors.grey,
+                      fontFamily: "BalooPaaji",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
