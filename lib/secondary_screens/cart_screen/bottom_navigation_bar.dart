6b9e8ba5fd5dcbc7bd9b4187f88f5365ad6b8257
lakshyaj46 <lakshyaj465@gmail.com>
2021-06-27 21:11:09 +0530

    Redesigned Cart Screen User Interface

--- lib/secondary_screens/cart_screen/bottom_navigation_bar.dart
+++ lib/secondary_screens/cart_screen/bottom_navigation_bar.dart
@@ -7,7 +7,6 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:help4you/models/user_model.dart';
 import 'package:help4you/services/database.dart';
 import 'package:help4you/models/cart_service_model.dart';
-import 'package:help4you/constants/signature_button.dart';
 
 class CartNavBar extends StatelessWidget {
   @override
@@ -16,58 +15,82 @@ class CartNavBar extends StatelessWidget {
     final user = Provider.of<Help4YouUser>(context);
 
     return Container(
+      height: MediaQuery.of(context).size.height * 0.13,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(30.0),
-          topRight: Radius.circular(30.0),
         ),
         color: Colors.white,
         boxShadow: [
           BoxShadow(
             offset: Offset(0, -15),
             blurRadius: 20.0,
-            color: Color(0xFFDADADA).withOpacity(0.15),
+            color: Color(0xFFDADADA).withOpacity(0.5),
           ),
         ],
       ),
-      child: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              StreamBuilder(
-                stream: DatabaseService(uid: user.uid).cartServiceData,
-                builder: (context, snapshot) {
-                  int total = 0;
-                  List<Help4YouCartServices> cartServices = snapshot.data;
-                  if (snapshot.connectionState == ConnectionState.active) {
-                    for (Help4YouCartServices cartService in cartServices) {
-                      total += cartService.servicePrice * cartService.quantity;
-                    }
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: StreamBuilder(
+              stream: DatabaseService(uid: user.uid).cartServiceData,
+              builder: (context, snapshot) {
+                int total = 0;
+                List<Help4YouCartServices> cartServices = snapshot.data;
+                if (snapshot.connectionState == ConnectionState.active) {
+                  for (Help4YouCartServices cartService in cartServices) {
+                    total += cartService.servicePrice * cartService.quantity;
                   }
-                  return Text(
-                    "₹$total",
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      color: Colors.black,
-                    ),
-                  );
-                },
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / (1792 / 1000),
-                child: SignatureButton(
-                  text: "Book Services",
-                  icon: FluentIcons.arrow_right_24_regular,
-                  onTap: () {
-                    // TODO: Implement Booking Of Services
-                  },
+                }
+                return Text(
+                  "₹$total",
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    color: Color(0xFF1C3857),
+                    fontFamily: "BalooPaaji",
+                    fontWeight: FontWeight.w600,
+                  ),
+                );
+              },
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              height: MediaQuery.of(context).size.height * 0.13,
+              width: MediaQuery.of(context).size.width * 0.6,
+              decoration: BoxDecoration(
+                color: Color(0xFF1C3857),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
                 ),
               ),
-            ],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Make Booking",
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.white,
+                      fontFamily: "BalooPaaji",
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Icon(
+                    FluentIcons.arrow_circle_right_24_filled,
+                    color: Colors.white,
+                    size: 28.0,
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
