213d7e681a7592683e9e54aeeb02369dca9817e4
lakshyaj46 <lakshyaj465@gmail.com>
2021-07-21 23:28:23 +0530

    Fixed iOS Issues (Debugging / Release / Profile)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +9,5 @@ import Firebase
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    FirebaseApp.configure()
   }
 }
