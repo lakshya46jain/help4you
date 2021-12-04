33db629670a1ac3c5d2bd02b70d3e5cc6666994e
lakshyaj46 <lakshyaj465@gmail.com>
2021-12-05 01:11:57 +0530

    Given Functionality to Filter Button

--- lib/screens/bookings_screen/bookings_screen.dart
+++ lib/screens/bookings_screen/bookings_screen.dart
@@ -1,5 +1,7 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 // Dependency Imports
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 // File Imports
@@ -11,7 +13,30 @@ class BookingsScreen extends StatefulWidget {
 }
 
 class _BookingsScreenState extends State<BookingsScreen> {
+  int index = 0;
   String bookingStatus;
+  FixedExtentScrollController scrollController;
+
+  final items = [
+    "Show All Bookings",
+    "Booking Pending",
+    "Job Completed",
+    "Accepted",
+    "Rejected",
+    "Cancelled",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = FixedExtentScrollController(initialItem: index);
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +57,61 @@ class _BookingsScreenState extends State<BookingsScreen> {
           Padding(
             padding: EdgeInsets.only(right: 5.0),
             child: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                scrollController.dispose();
+                scrollController =
+                    FixedExtentScrollController(initialItem: index);
+                final pickerOptions = Container(
+                  height: 225.0,
+                  child: CupertinoPicker(
+                    scrollController: scrollController,
+                    itemExtent: 54,
+                    onSelectedItemChanged: (index) {
+                      HapticFeedback.lightImpact();
+                      setState(() => this.index = index);
+                      if (index == 0) {
+                        setState(() {
+                          bookingStatus = null;
+                        });
+                      } else if (index == 1) {
+                        setState(() {
+                          bookingStatus = "Booking Pending";
+                        });
+                      } else if (index == 2) {
+                        setState(() {
+                          bookingStatus = "Job Completed";
+                        });
+                      } else if (index == 3) {
+                        setState(() {
+                          bookingStatus = "Accepted";
+                        });
+                      } else if (index == 4) {
+                        setState(() {
+                          bookingStatus = "Rejected";
+                        });
+                      } else if (index == 5) {
+                        setState(() {
+                          bookingStatus = "Cancelled";
+                        });
+                      }
+                    },
+                    children: items
+                        .map(
+                          (item) => Center(
+                            child: Text(
+                              item,
+                              style: TextStyle(fontSize: 22.0),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                );
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) => pickerOptions,
+                );
+              },
               icon: Icon(
                 FluentIcons.filter_16_filled,
                 color: Color(0xFF1C3857),
